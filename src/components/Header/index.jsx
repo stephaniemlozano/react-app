b7e2d663b1f7368e80317bc96d1c8b54c2b74001
Stephanie Sadowski <stephaniesadowski@Stephanies-Air.home>
2022-08-11 21:28:54 -0400

    created Header component and some styling

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './styles.css'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+const Header = () => {
+  return (
+    <Navbar sticky='top' className='nav-custom' bg='light' variant='light'>
+      <Navbar.Brand href='#home'>S T E P H A N I E</Navbar.Brand>
+
+      <Nav className='mr-auto nav-content '>
+        <Nav.Link href='#home'>My Story</Nav.Link>
+        <Nav.Link href='#features'>Projects</Nav.Link>
+        <Nav.Link href='#pricing'>Connect</Nav.Link>
+      </Nav>
+    </Navbar>
+  )
+}
+
+export default Header
